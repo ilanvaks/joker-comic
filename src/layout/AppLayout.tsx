97edcfab4b0f4dcfdd4c7862ca75a9a3b3db150a
IlanVaks <ilanvaks@gmail.com>
2023-06-20 17:47:18 -0400

    layout-ts

---
+++ src/layout/AppLayout.tsx
@@ -0,0 +1,18 @@
+import {ReactNode} from "react";
+import Footer from "./Footer";
+
+interface AppLayoutProps {
+  children: ReactNode;
+}
+
+const AppLayout = ({children}: AppLayoutProps) => {
+
+  return ( 
+    <>
+    {children}
+    <Footer/>
+    </>
+  )
+}
+
+export default AppLayout;
---
+++ src/layout/Footer/Footer.jsx
@@ -0,0 +1,19 @@
+import { Container } from "react-bootstrap";
+
+
+
+export default function Footer () {
+
+
+  return ( 
+    <footer>
+      <Container>
+        <Row>
+          <Col>
+          <p>Footer</p>
+          </Col>
+        </Row>
+      </Container>
+    </footer>
+  )
+} 
