81585e78f06961dff2185d37dcd8d3d175753acb
IlanVaks <ilanvaks@gmail.com>
2023-06-27 16:08:29 -0400

    fixed-image-routes

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,28 @@
-import { Container, Row, Col, Image } from "react-bootstrap";
+import { Container, Row, Col, Image, Modal, Button } from "react-bootstrap";
 import AppLayout from "../layout/AppLayout";
+import React, { useState } from 'react';
 
-const Home = () => {
+interface ImageWithModalProps {
+  src: string;
+}
+
+const ImageWithModal: React.FC<ImageWithModalProps> = ({src}) => {
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
+  return (
+    <>
+      <Image fluid className="glowing-border" src={src} onClick={handleShow} />
+      <Modal show={show} onHide={handleClose}>
+         <Image fluid src={src} />
+      </Modal>
+    </>
+  );
+}
+
+const Home: React.FC = () => {
   const imageUri = process.env.PUBLIC_URL + '/images/';
 
   return (
@@ -9,32 +30,35 @@ const Home = () => {
       <section>
         <Container fluid className="joker-cover">
           <Row className="g-0">
-            <Col lg={4} className="less-padding"><Image fluid className="glowing-border" src={imageUri + "jokerTrying.jpg"} /></Col>
-            <Col lg={4} className="less-padding"><Image fluid className="glowing-border" src={imageUri + "JokerStandUp.jpg"} /></Col>
-            <Col lg={4} ><Image fluid className="glowing-border" src={imageUri + "emptyAud.jpg"} /></Col>
-          </Row>
-          
-          <Row className="g-0">
-            <Col lg={4}><Image fluid className="glowing-border" src={imageUri + "jokerMirror.jpg"} /></Col>
-            <Col lg={4}><Image fluid className="glowing-border" src={imageUri + "JokerCover.jpg"} /></Col>
-            <Col lg={4}><Image fluid className="glowing-border" src={imageUri + "Joker-Charge.jpg"} /></Col>
+            <Col lg={4} className="less-padding"><ImageWithModal src={imageUri + "jokerTrying.jpg"} /></Col>
+            <Col lg={4} className="less-padding"><ImageWithModal src={imageUri + "JokerStandUP.jpg"} /></Col>
+            <Col lg={4}><ImageWithModal src={imageUri + "emptyAud.jpg"} /></Col>
           </Row>
 
           <Row className="g-0">
-            <Col lg={4}><Image fluid className="glowing-border" src={imageUri + "BurnDownTown.jpg"} /></Col>
-            <Col lg={4}><Image fluid className="glowing-border" src={imageUri + "BatManLabWatch.jpg"} /></Col>
-            <Col lg={4}><Image fluid className="glowing-border" src={imageUri + "BatManLab2.jpg"} /></Col>
+            <Col lg={4}><ImageWithModal src={imageUri + "jokerMirror.jpg"} /></Col>
+            <Col lg={4}><ImageWithModal src={imageUri + "jokerCover.jpg"} /></Col>
+            <Col lg={4}><ImageWithModal src={imageUri + "Joker-Charge.jpg"} /></Col>
           </Row>
+
           <Row className="g-0">
-            <Col lg={4}><Image fluid className="glowing-border" src={imageUri + "BatMobile.jpg"} /></Col>
-            <Col lg={4}><Image fluid className="glowing-border" src={imageUri + "BatMobile2.jpg"} /></Col>
-            <Col lg={4}><Image fluid className="glowing-border" src={imageUri + "batmanFight.jpg"} /></Col>
+            <Col lg={4}><ImageWithModal src={imageUri + "burnDownTown.jpg"} /></Col>
+            <Col lg={4}><ImageWithModal src={imageUri + "BatManLabWatch.jpg"} /></Col>
+            <Col lg={4}><ImageWithModal src={imageUri + "BatManLab2.jpg"} /></Col>
           </Row>
+
           <Row className="g-0">
-            <Col lg={4}><Image fluid className="glowing-border" src={imageUri + "jokerStreets.jpg"} /></Col>
-            <Col lg={4}><Image fluid className="glowing-border" src={imageUri + "BatmanStandOFF.jpg"} /></Col>
-            <Col lg={4}><Image fluid className="glowing-border" src={imageUri + "JokerStandOFF.jpg"} /></Col>
+            <Col lg={4}><ImageWithModal src={imageUri + "BatMobile.jpg"} /></Col>
+            <Col lg={4}><ImageWithModal src={imageUri + "batmobile2.jpg"} /></Col>
+            <Col lg={4}><ImageWithModal src={imageUri + "batmanFight.jpg"} /></Col>
           </Row>
+
+          <Row className="g-0">
+            <Col lg={4}><ImageWithModal src={imageUri + "jokerStreets.jpg"} /></Col>
+            <Col lg={4}><ImageWithModal src={imageUri + "BatmanStandOFF.jpg"} /></Col>
+            <Col lg={4}><ImageWithModal src={imageUri + "JokerStandOFF.jpg"} /></Col>
+          </Row>
+
           <Row>
         <a href="https://www.youtube.com/watch?v=wcinzmfZeCc" target="_blank" rel="noopener noreferrer" className="no-underline">
           <p className="text-center" style={{ 
