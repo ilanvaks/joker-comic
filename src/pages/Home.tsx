9cdc1cc2956189aef101432f1fddae78b361b6cf
IlanVaks <ilanvaks@gmail.com>
2023-06-29 14:56:01 -0400

    connected-api

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,7 @@
-import { Container, Row, Col, Image, Modal, Button } from "react-bootstrap";
+import React, { useEffect, useState } from 'react';
+import { Container, Row, Col, Image, Modal } from "react-bootstrap";
 import AppLayout from "../layout/AppLayout";
 import OffCanvasQuiz from "./Cover"
-import React, { useState } from 'react';
 
 interface ImageWithModalProps {
   src: string;
@@ -24,7 +24,13 @@ const ImageWithModal: React.FC<ImageWithModalProps> = ({src}) => {
 }
 
 const Home: React.FC = () => {
-  const imageUri = process.env.PUBLIC_URL + '/images/';
+  const [images, setImages] = useState<string[]>([]);
+
+  useEffect(() => {
+    fetch('https://my-comic-book-iv.web.app/joker')
+      .then((response) => response.json())
+      .then((data) => setImages(data.map((item: any) => item.image)));
+  }, []);
 
   return (
     <AppLayout>
@@ -34,51 +40,27 @@ const Home: React.FC = () => {
       <section>
         <Container fluid className="joker-cover">
           <Row className="g-0">
-            <Col lg={4} className="less-padding"><ImageWithModal src={imageUri + "jokerTrying.jpg"} /></Col>
-            <Col lg={4} className="less-padding"><ImageWithModal src={imageUri + "JokerStandUP.jpg"} /></Col>
-            <Col lg={4}><ImageWithModal src={imageUri + "emptyAud.jpg"} /></Col>
-          </Row>
-
-          <Row className="g-0">
-            <Col lg={4}><ImageWithModal src={imageUri + "jokerMirror.jpg"} /></Col>
-            <Col lg={4}><ImageWithModal src={imageUri + "jokerCover.jpg"} /></Col>
-            <Col lg={4}><ImageWithModal src={imageUri + "Joker-Charge.jpg"} /></Col>
-          </Row>
-
-          <Row className="g-0">
-            <Col lg={4}><ImageWithModal src={imageUri + "burnDownTown.jpg"} /></Col>
-            <Col lg={4}><ImageWithModal src={imageUri + "BatmanLabWatch.jpg"} /></Col>
-            <Col lg={4}><ImageWithModal src={imageUri + "BatmanLab2.jpg"} /></Col>
-          </Row>
-
-          <Row className="g-0">
-            <Col lg={4}><ImageWithModal src={imageUri + "BatMobile.jpg"} /></Col>
-            <Col lg={4}><ImageWithModal src={imageUri + "batmobile2.jpg"} /></Col>
-            <Col lg={4}><ImageWithModal src={imageUri + "batmanFight.jpg"} /></Col>
-          </Row>
-
-          <Row className="g-0">
-            <Col lg={4}><ImageWithModal src={imageUri + "jokerStreets.jpg"} /></Col>
-            <Col lg={4}><ImageWithModal src={imageUri + "BatmanStandOFF.jpg"} /></Col>
-            <Col lg={4}><ImageWithModal src={imageUri + "JokerStandOFF.jpg"} /></Col>
+            {images.map((src, index) => (
+              <Col key={index} lg={4} className="less-padding">
+                <ImageWithModal src={src} />
+              </Col>
+            ))}
           </Row>
 
           <Row>
-        <a href="https://www.youtube.com/watch?v=wcinzmfZeCc" target="_blank" rel="noopener noreferrer" className="no-underline">
-          <p className="text-center" style={{ 
-            color: 'red', 
-            fontSize: '40px', 
-            fontWeight: 'bold', 
-            fontFamily: '"Comic Sans MS", cursive, sans-serif',
-            textShadow: '2px 2px 4px #000000',
-            textAlign: 'center'
-          }}> 
-            TO BE CONTINUED ...
-          </p>
-        </a>
-
-</Row>
-
+            <a href="https://www.youtube.com/watch?v=wcinzmfZeCc" target="_blank" rel="noopener noreferrer" className="no-underline">
+              <p className="text-center" style={{ 
+                color: 'red', 
+                fontSize: '40px', 
+                fontWeight: 'bold', 
+                fontFamily: '"Comic Sans MS", cursive, sans-serif',
+                textShadow: '2px 2px 4px #000000',
+                textAlign: 'center'
+              }}> 
+                TO BE CONTINUED ...
+              </p>
+            </a>
+          </Row>
         </Container>
       </section>
     </AppLayout>
